40934f3b7b1bb191c46107948964d4c418fea8b1
salman2001 <salmankhan547@gmail.com>
2021-02-07 16:58:26 +0000

    account

--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -1,7 +1,33 @@
 <template>
   <div class="about">
-    <h1>This is an accounts page</h1>
+    <h1>Your Account</h1>
+    
   </div>
+<h2 class = "header">Welcome to your Profile</h2>
+
+  <body>
+    
+    <div class = "profile">
+      <div>Your Details are as Below. If you wish to change them please fill the change of details form</div>
+<div><b>Email:</b></div>
+<div><b>Username :</b> </div>
+<div><b>Name :</b></div>
+<div><b>Institution:</b></div>
+<div><b>Address:</b></div>
+<div> <b>Contact Detail: </b></div>
+</div>
+<b>
+<div><h1 class = "change">Change of Detail Form</h1></div>
+  </b>
+<div class="edit">To edit your details please fill out the form below</div>
+    <div class= "editForm"><div><b>Email:</b></div>
+<div><b>Username :</b> </div>
+<div><b>Name :</b></div>
+<div><b>Institution:</b></div>
+<div><b>Address:</b></div>
+<div> <b>Contact Detail: </b></div>
+</div>
+  </body>
 </template>
 
 <script>
@@ -16,5 +42,25 @@ export default {
 </script>
 
 <style scoped>
+.profile{
+  text-align: left;
+}
+.about{
+  text-align: middle;
+  font-size: 20px;
+}
+.header{
+  text-align: left;
+}
+.edit{
+  text-align: left;
+}
+.editForm{
+  text-align: left;
+}
+.change{
+  text-align: left;
+  font-size: 25px;
+}
 
 </style>
