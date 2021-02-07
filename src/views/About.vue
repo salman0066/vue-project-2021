209fe4278075e3cfdaadee21863588d26455ba16
ClientAd-HonourShelgon408 <HonourShelgon408@gmail.com>
2021-02-07 13:06:45 +0000

    Added script and style section to the views

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,18 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+
+// import {ref} from 'vue';
+
+export default {
+  setup(){
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/MyGraphs.vue
+++ src/views/MyGraphs.vue
@@ -3,3 +3,18 @@
     <h1>This is a graph page</h1>
   </div>
 </template>
+
+<script>
+
+// import {ref} from 'vue';
+
+export default {
+  setup(){
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
