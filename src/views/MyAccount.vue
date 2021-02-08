4d55364c5c6cbf441fcee6c70f7419b27216ce2b
salman2001 <salmankhan547@gmail.com>
2021-02-08 13:38:46 +0000

    updatedversion

--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -9,24 +9,60 @@
     
     <div class = "profile">
       <div>Your Details are as Below. If you wish to change them please fill the change of details form</div>
-<div><b>Email:</b></div>
-<div><b>Username :</b> </div>
-<div><b>Name :</b></div>
-<div><b>Institution:</b></div>
-<div><b>Address:</b></div>
-<div> <b>Contact Detail: </b></div>
+<div>Email:</div>
+<div>Username : </div>
+<div>Name :</div>
+<div>Institution:</div>
+<div>Address:</div>
+<div>Contact Detail: </div>
 </div>
 <b>
 <div><h1 class = "change">Change of Detail Form</h1></div>
   </b>
 <div class="edit">To edit your details please fill out the form below</div>
-    <div class= "editForm"><div><b>Email:</b></div>
-<div><b>Username :</b> </div>
-<div><b>Name :</b></div>
-<div><b>Institution:</b></div>
-<div><b>Address:</b></div>
-<div> <b>Contact Detail: </b></div>
+<form >
+  <div class= "editForm">
+    <div><label>Email</label>
+    <input required type="email" placeholder="email">
+    </div>
+<div>
+
+  <label>Username</label>
+  <input required type="userName" placeholder="username"> </div>
+
+
+<div>
+  <label>First Name</label>
+  <input required type="firstName" placeholder="first name">
 </div>
+<div>
+  <label>Last Name</label>
+  <input placeholder="last name">
+</div>
+
+
+<div>
+  <label>Institution</label>
+  <input placeholder="institution">
+</div>
+
+
+<div>
+  <label>Address</label>
+  <input placeholder="address">
+</div>
+
+
+<div> 
+  <label>Contact Details</label>
+  <input placeholder="Contact Details">
+</div>
+
+
+</div>
+<input class="submitBtn" type="submit" value="Submit">
+</form>
+    
   </body>
 </template>
 
@@ -61,6 +97,9 @@ export default {
 .change{
   text-align: left;
   font-size: 25px;
+}
+.submitBtn{
+  place-content: left;
 }
 
 </style>
