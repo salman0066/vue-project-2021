4fef6b47c201d99784e48a18431eb3f561252ebb
salman2001 <salmankhan547@gmail.com>
2021-02-13 15:12:19 +0000

    Update uploadData.vue

--- src/views/uploadData.vue
+++ src/views/uploadData.vue
@@ -1,16 +1,43 @@
 <template>
-  <div class="data">
-    <h1>user can add his or her data here.</h1>
-  </div>
+  <div style="margin:auto; text-align:center;"><h2>Add Data to Database</h2></div>
+
+  <el-form label-width="120px">
+    
+      <el-form-item label="Title">
+        <el-input type="text" v-model="title" placeholder="Enter title" autocomplete="off" required></el-input>
+      </el-form-item>
+
+      <el-form-item label="Description">
+        <el-input type=" text" v-model="description" placeholder="Enter description" autocomplete="off" required></el-input>
+      </el-form-item>
+
+      <el-form-item label="Content">
+        <el-input type="text" v-model="content" placeholder="Enter content" autocomplete="off" required></el-input>
+      </el-form-item>
+
+      <el-form-item label="Tags">
+        <el-input type="text" v-model="tags" placeholder="Enter tags" autocomplete="off" required></el-input>
+      </el-form-item>
+      
+      <el-form-item>
+        <el-button type="success" @click="addPost">Add Data</el-button>
+       
+      </el-form-item>
+  </el-form>
 </template>
 
 <script>
 
-// import {ref} from 'vue';
+ import {ref} from 'vue';
 
 export default {
   setup(){
 
+    const title = ref("");
+    const description = ref("")
+    const content = ref("")
+    const tags = ref("")
+    return {title, description, content, tags}
   }
 }
 </script>
