1dc6e4904a1072e6b41f7d404a06986eacaf1585
bilaalmusa <>
2021-02-18 17:19:20 +0000

    Update MyAccount.vue

--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -1,53 +1,87 @@
 <template>
-   <div class="about">
-      <h1>Your Account</h1>
-   </div>
-   <h2 class = "header">Welcome to your Profile</h2>
-   <body>
-      <div class = "profile">
-         <div>Your Details are as Below. If you wish to change them please fill the change of details form</div>
-         <div>
-            <el-card class="box-card">
-               <div v-if="user">
-                  <div>
-                     Welcome Back
-                     <span style = "color: green; font-weight: bold"> {{ user }} </span> <br>
-                     <el-button type="text" icon="el-icon-user" @click="$emit('logout')">Logout</el-button>
-                  </div>
-               </div>
-            </el-card>
-         </div>
-         <div>Email : {{userDetails.email}}</div>
-         <div>Name :  {{userDetails.fullName}}</div>
-         <div>Institution:  {{userDetails.institution}}</div>
-         <div>Address: {{userDetails.address}}</div>
-         <div>Contact Detail: {{userDetails.contact_details}} </div>
+  <div class="about">
+    <h1>Your Account</h1>
+  </div>
+  <h2 class="header">Welcome to your Profile</h2>
+  <body>
+    <div class="profile">
+      <div>
+        Your Details are as Below. If you wish to change them please fill the
+        change of details form
       </div>
-      <b>
-         <div>
-            <h1>Change of Detail Form</h1>
-         </div>
-      </b>
-      <div>To edit your details please fill out the form below</div>
-      <el-form >
-         <el-form-item label="Username">
-            <el-input v-model="username" placeholder="Enter username" autocomplete="off" required></el-input>
-         </el-form-item>
-         <el-form-item label="Name">
-            <el-input v-model="fullName" placeholder="name" autocomplete="off" required></el-input>
-         </el-form-item>
-         <el-form-item label="Institution">
-            <el-input v-model="institution" placeholder="Enter title" autocomplete="off" required></el-input>
-         </el-form-item>
-         <el-form-item label="Address">
-            <el-input v-model="address" placeholder="Enter title" autocomplete="off" required></el-input>
-         </el-form-item>
-         <el-form-item label="Contact Details">
-            <el-input v-model="contact_details" placeholder="Enter title" autocomplete="off" required></el-input>
-         </el-form-item>
-         <el-button>Update Details</el-button>
-      </el-form>
-   </body>
+      <div>
+        <el-card class="box-card">
+          <div v-if="user">
+            <div>
+              Welcome Back
+              <span style="color: green; font-weight: bold"> {{ user }} </span>
+              <br />
+              <el-button
+                type="text"
+                icon="el-icon-user"
+                @click="$emit('logout')"
+                >Logout</el-button
+              >
+            </div>
+          </div>
+        </el-card>
+      </div>
+      <div>Email : {{userDetails.email}}</div>
+      <div>Name : {{userDetails.fullName}}</div>
+      <div>Institution: {{userDetails.institution}}</div>
+      <div>Address: {{userDetails.address}}</div>
+      <div>Contact Detail: {{userDetails.contact_details}}</div>
+    </div>
+    <b>
+      <div>
+        <h1>Change of Detail Form</h1>
+      </div>
+    </b>
+    <div>To edit your details please fill out the form below</div>
+    <el-form>
+      <el-form-item label="Username">
+        <el-input
+          v-model="username"
+          placeholder="Enter username"
+          autocomplete="off"
+          required
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="Name">
+        <el-input
+          v-model="fullName"
+          placeholder="name"
+          autocomplete="off"
+          required
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="Institution">
+        <el-input
+          v-model="institution"
+          placeholder="Enter title"
+          autocomplete="off"
+          required
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="Address">
+        <el-input
+          v-model="address"
+          placeholder="Enter title"
+          autocomplete="off"
+          required
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="Contact Details">
+        <el-input
+          v-model="contact_details"
+          placeholder="Enter title"
+          autocomplete="off"
+          required
+        ></el-input>
+      </el-form-item>
+      <el-button>Update Details</el-button>
+    </el-form>
+  </body>
 </template>
 
 <script>
@@ -126,28 +160,27 @@ export default {
 </script>
 
 <style scoped>
-.profile{
+.profile {
   text-align: left;
 }
-.about{
+.about {
   text-align: middle;
   font-size: 20px;
 }
-.header{
+.header {
   text-align: left;
 }
 
-button:hover {   
-        opacity: 0.9;   
-    }  
-button {   
+button:hover {
+  opacity: 0.9;
+}
+button {
   display: inline-block;
-       background-color:black;   
-       width: 50%;  
-        color: white;   
-        padding: 15px;   
-        margin: 10px 0px;     
-        cursor: pointer;   
-         }   
-
+  background-color: black;
+  width: 50%;
+  color: white;
+  padding: 15px;
+  margin: 10px 0px;
+  cursor: pointer;
+}
 </style>
