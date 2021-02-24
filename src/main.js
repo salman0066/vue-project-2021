a42042a6a001caf8eab3c7c919c493e99ff1c464
Vikki Hanley <vikki.hanley1997@gmail.com>
2021-02-24 01:18:17 +0000

    Data Upload
    Data upload is now working, using the bottom boxes this puts the data into an array in firebase and a drop down helps a guided search for the client. Next steps:
    -Adding a 'add more' button for array so it looks less messy and clients can add as many points as they like
    -Getting the userID box automatically filled based on the person logged in(will then remove this from the form).
    
    The styling using bootstrap isnt working on firefox for some reason.. unsure why.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router';
 import VueApexCharts from "vue3-apexcharts";
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"></link>
 
 const app = createApp(App);
 app.use(router);
--- src/views/uploadData.vue
+++ src/views/uploadData.vue
@@ -1,47 +1,159 @@
 <template>
-  <div style="margin:auto; text-align:center;"><h2>Add Data to Database</h2></div>
+    <div class="addData">
+      <h1>Add Data</h1>
+            <form @submit.prevent="onFormSubmit">
+              <div class= "addDataForm">
+                <div class = "form-group">
+                    <label>Gene Type</label>
+                    <select class="form-control" v-model="data.data_type" required>
+                      <option>TNNT</option>
+                      <option>MYH</option>
+                      <option>MYBPC3</option>
+                      <option>TPM1</option>
+                    </select>
+                </div>
 
-  <el-form label-width="120px">
-    
-      <el-form-item label="Title">
-        <el-input v-model="title" placeholder="Enter title" autocomplete="off" required></el-input>
-      </el-form-item>
+                <div class = "form-group">
+                    <label>Chart Title</label>
+                    <input type="text" class="form-control" v-model="data.title" required>
+                </div>
 
-      <el-form-item label="Description">
-        <el-input  v-model="description" placeholder="Enter description" autocomplete="off" required></el-input>
-      </el-form-item>
+                <div class = "form-group">
+                    <label>User ID</label>
+                    <input type="text" class="form-control" v-model="data.uid_source" required>
+                </div>
 
-      <el-form-item label="Content">
-        <el-input v-model="content" placeholder="Enter content" autocomplete="off" required></el-input>
-      </el-form-item>
+                <div class = "form-group">
+                    <label>Tags</label>
+                    <input type="text" class="form-control" v-model="data.tags" required>
+                </div>
+          <div class="container">
+              <div class="row">
+                <div class='col' >
+                  <label>X Axis Label</label>
+                    <input type="text" class="form-control" v-model="data.x_label" required>
+                    <label>X Axis Data</label>
+                    <input type="text" class="form-control-sm" name="x_data[]" v-model="data.x_data[0]" >
+                    <input type="text" class="form-control-sm" name="x_data[]" v-model="data.x_data[1]" >
+                    <input type="text" class="form-control-sm" name="x_data[]" v-model="data.x_data[2]" >
+                    <input type="text" class="form-control-sm" name="x_data[]" v-model="data.x_data[3]" >
+                    <input type="text" class="form-control-sm" name="x_data[]" v-model="data.x_data[4]" >
+                    <input type="text" class="form-control-sm" name="x_data[]" v-model="data.x_data[5]" >
+                    <input type="text" class="form-control-sm" name="x_data[]" v-model="data.x_data[6]" >
+                    <input type="text" class="form-control-sm" name="x_data[]" v-model="data.x_data[7]" >
+                    <input type="text" class="form-control-sm" name="x_data[]" v-model="data.x_data[8]" >
+                    <input type="text" class="form-control-sm" name="x_data[]" v-model="data.x_data[9]" >
+                    <input type="text" class="form-control-sm" name="x_data[]" v-model="data.x_data[10]" >
 
-      <el-form-item label="Tags">
-        <el-input  v-model="tags" placeholder="Enter tags" autocomplete="off" required></el-input>
-      </el-form-item>
-      
-      <el-form-item>
-        <el-button type="success" @click="addPost">Add Data</el-button>
-       
-      </el-form-item>
-  </el-form>
+
+
+                </div>
+
+                <div class='col'>
+                  <label>Y Axis Label</label>
+                    <input type="text" class="form-control" v-model="data.y_label" required>
+                    <label>Y Axis Data</label>
+                    <input type="text" class="form-control-sm" name="y_Data[]" v-model="data.y_Data[0]" >
+                    <input type="text" class="form-control-sm" name="y_Data[]" v-model="data.y_Data[1]" >
+                    <input type="text" class="form-control-sm" name="y_Data[]" v-model="data.y_Data[2]" >
+                    <input type="text" class="form-control-sm" name="y_Data[]" v-model="data.y_Data[3]" >
+                    <input type="text" class="form-control-sm" name="y_Data[]" v-model="data.y_Data[4]" >
+                    <input type="text" class="form-control-sm" name="y_Data[]" v-model="data.y_Data[5]" >
+                    <input type="text" class="form-control-sm" name="y_Data[]" v-model="data.y_Data[6]" >
+                    <input type="text" class="form-control-sm" name="y_Data[]" v-model="data.y_Data[7]" >
+                    <input type="text" class="form-control-sm" name="y_Data[]" v-model="data.y_Data[8]" >
+                    <input type="text" class="form-control-sm" name="y_Data[]" v-model="data.y_Data[9]" >
+                    <input type="text" class="form-control-sm" name="y_Data[]" v-model="data.y_Data[10]" >
+                </div>
+              </div>
+          </div>
+
+                <div class="form-group">
+                    <button class="btn btn-primary btn-block">Add Data</button>
+                </div>
+              </div>
+            </form>
+  
+
+    </div>
 </template>
 
 <script>
-
-import {ref} from 'vue';
+//import {ref} from "vue";
+import { firebaseFireStore } from "@/firebase/database";
 
 export default {
-  setup(){
+  data() {
+    return {
+      data: {
+        x_data: [],
+        y_Data: []
+    }
+  }
+},
 
-    const title = ref("");
-    const description = ref("")
-    const content = ref("")
-    const tags = ref("")
-    return {title, description, content, tags}
+methods: {
+  onFormSubmit(event) {
+    event.preventDefault();
+    firebaseFireStore.collection('data').add(this.data).then(() => {
+      alert("Data successfully added!");
+      this.data.data_type = ''
+      this.data.title = ''
+      this.data.uid_source = ''
+      this.data.x_data = ''
+      this.data.x_label = ''
+      this.data.y_Data = ''
+      this.data.y_label = ''
+      this.data.tags = ''
+    }).catch((error) => {
+      console.log(error);
+    });
+    },
+
+    add() {
+      this.data.push({
+        x_data: '',
+        y_Data: ''
+      })
+    }
   }
 }
+
 </script>
 
 <style scoped>
+
+button {   
+       background-color: #C1272D;  
+       align-content: center; 
+        color: white;  
+        cursor: pointer;   
+         }   
+
+ input[type=text]{   
+        width: 100%;   
+        margin: 8px 0;  
+        padding: 12px 20px;   
+        display: inline-block;     
+        box-sizing: border-box;   
+    }  
+ button:hover {   
+        opacity: 0.7;   
+    }   
+ 
+ .container {   
+        padding: 25px;  
+        font-size: 15px; 
+    }
+  
+.arrayInput{
+        display: inline-block;
+        color: #C1272D;
+}
+
+.addDataForm{
+  align-content: center;
+  margin: 5%;
+}
 
 </style>
