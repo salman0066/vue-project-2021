7607db9396b5216bbe76fa0cc012647bca527d85
salman2001 <salmankhan547@gmail.com>
2021-02-25 17:01:24 +0000

    api not fully working thou

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -5,33 +5,35 @@
 <h2 class = "header"> Use this page to search all data</h2>
 <body>
 <div class="searchInput">
-<el-form >
+<el-form @submit.prevent="populateReturns">
     <el-form-item label="Enter Search Term">
     <el-input type="text" placeholder="Search term.." v-model="search" required></el-input>
     
+    <button type="submit">Submit </button>
     </el-form-item>
 </el-form>
 </div>  
 <div v-if="error">{{error}}</div>
-<el-row v-if="search_returns.length">
+<el-row v-if="diseaseData">
     <el-col 
     :span="8" 
-    v-for="disease in diseases"
-    :key="disease.id"
+    v-for="disease in diseaseData.catTermsMap"
+    :key="disease"
     :offset="index > 0 ? 2 : 0">
     
 
-    <el-card :body-style="{ padding: '8px'}">"
+    <el-card :body-style="{ padding: '8px'}">
         <template #header>
          <div class = "searchHeader">
-            <span>{{ diseaseId }}</span>
+             <h3>{{disease.terms[0].name}}</h3>
+            <span>{{ disease.terms[0].definition }}</span>
         </div>
-    </template>>
+    </template>
     <div style="padding: 14px;" class="bottom">
         <span>{{ diseaseName}}</span>        
     </div>
      </el-card>
-    </el-col>>
+    </el-col>
     </el-row>
     </body> 
     </div>  
@@ -39,7 +41,7 @@
 
 <script>
 
-import {ref} from 'vue';
+import {computed, ref} from 'vue';
 
 export default {
 props: {
@@ -57,18 +59,22 @@ let search_returns = ref([]);
 const error = ref(null);
 const diseaseId = search;
 
-const base_url = "hpo.jax.org";
-
+//const base_url = "https://hpo.jax.org";
+const data = ref();
 async function populateReturns(){
-const search_base_url = base_url +"api/hpo/disease/"+{diseaseId}+""
+//const search_base_url = base_url +`api/hpo/disease/OMIM${diseaseId.value}`
+console.log(diseaseId.value)
 
 try {
-let search_data = await fetch(search_base_url);
+let search_data = await fetch(`https://hpo.jax.org/api/hpo/disease/OMIM%3A${diseaseId.value}`);
 if(!search_data.ok){
     throw Error("Unable to find anything")
     }
 
-    search.value = await search_data.json();
+    data.value = await search_data.json();
+    console.log(data.value);
+
+  
 }
 
 catch (search_error) {
@@ -76,9 +82,13 @@ catch (search_error) {
     console.log(error.value);
 }
 }
-populateReturns();
 
-return {search, search_returns, populateReturns}
+  const diseaseData = computed(() => {
+        return data.value;
+    })
+//populateReturns();
+
+return {search, search_returns, populateReturns, diseaseData}
 },
 };
 </script>
