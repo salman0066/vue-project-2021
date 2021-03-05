7b8a2cf45d7829f8dd17e5626989624f16e8c424
ClientAd-HonourShelgon408 <honourshelgon408@gmail.com>
2021-03-05 16:27:56 +0000

    Removed file

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,13 +1,12 @@
-import { mount, shallowMount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
-  test('renders props.msg when passed', () => {
-    const msg = 'new message';
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
       props: { msg }
     })
-    expect(wrapper.text()).toMatch(msg);
-    // expect(wrapper.find("h1").text()).toBe(msg);
+    expect(wrapper.text()).toMatch(msg)
   })
 })
