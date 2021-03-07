36c45cfa46d2bb5b68573cb037e4aca8b508a507
salman2001 <salmankhan547@gmail.com>
2021-03-07 14:02:52 +0000

    footer
    changed the about to help

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-fluid footer">
     <footer id="footer">
-        <router-link :to="{ name: 'About'}">About Us | </router-link> 
+        <router-link :to="{ name: 'About'}">Help | </router-link> 
         <router-link :to="{ name: 'ContactUs'}">Contact Us</router-link> 
         <p><a href="https://github.com/AWD-Group-6/vue-project-2021">Check the app out on GitHub</a></p>
   </footer>
