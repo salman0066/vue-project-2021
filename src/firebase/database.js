2e8bf08cb8449c5bcdbc7480a8a650f727bedb55
salman2001 <salmankhan547@gmail.com>
2021-03-08 11:35:07 +0000

    Database changed

--- src/firebase/database.js
+++ src/firebase/database.js
@@ -4,14 +4,13 @@ import "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBWx4p0j16mdUIivZLBqz_8l_BH6OhRpR4",
-    authDomain: "myocardio-awdg6.firebaseapp.com",
-    projectId: "myocardio-awdg6",
-    storageBucket: "myocardio-awdg6.appspot.com",
-    messagingSenderId: "886784373926",
-    appId: "1:886784373926:web:08305df28d2701e4c3b067",
-    measurementId: "G-PK42GS9GFN"
-  };
+  apiKey: "AIzaSyBv9vv6V8HP6SSNae-oMyqmRaXVayEsq1o",
+  authDomain: "myo-cardio066.firebaseapp.com",
+  projectId: "myo-cardio066",
+  storageBucket: "myo-cardio066.appspot.com",
+  messagingSenderId: "963418076647",
+  appId: "1:963418076647:web:4e609c1d55cc018101b942"
+};
 
   //initialize firebase
   firebase.initializeApp(firebaseConfig);
